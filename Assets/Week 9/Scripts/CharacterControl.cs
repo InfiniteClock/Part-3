d4ae6ef89a5b1ffffe5799907160a787022c8401
Bryce Davie <daviebry@shernet.sheridancollege.ca>
2024-03-13 16:53:20 -0400

    Task 4 complete: Thief implemented with two dagger throwing action and dash on attack. UI added to show selected unit's name, or no selection

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -2,18 +2,35 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CharacterControl : MonoBehaviour
 {
+    public TMP_Text textUI;
+    public static string selection;
     public static Villager SelectedVillager { get; private set; }
+    private void Start()
+    {
+        //If there were a way to not have a villager selected after selecting one, I would implement this differently
+        selection = "No Selection";
+    }
+    private void Update()
+    {
+        textUI.text = selection;
+    }
     public static void SetSelectedVillager(Villager villager)
     {
         if(SelectedVillager != null)
         {
             SelectedVillager.Selected(false);
         }
+        else
+        {
+            //There does not exist a deselection method to call this, but if there were, then this would help
+            selection = "No Selection";
+        }
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
     }
-    
+
 }
