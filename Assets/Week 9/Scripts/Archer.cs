3a9395e5a8d88982e88491cf8968156e4a75d75d
Bryce Davie <daviebry@shernet.sheridancollege.ca>
2024-03-13 13:40:49 -0400

    Task 1b complete: Archer derivative of Villager created and added to archer object. Archer has arrow prefab and spawnpoint to shoot arrows when overriding Attack()

---
+++ Assets/Week 9/Scripts/Archer.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Archer : Villager
+{
+    public GameObject arrow;
+    public Transform spawnPoint;
+
+    protected override void Attack()
+    {
+        destination = transform.position;
+        Instantiate(arrow, spawnPoint);
+    }
+}
