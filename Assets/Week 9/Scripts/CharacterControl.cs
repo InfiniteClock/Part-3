673f2be0bedb313c8e4deb60122783552da8d935
Bryce Davie <daviebry@shernet.sheridancollege.ca>
2024-03-20 17:16:01 -0400

    Task 4a complete: Added world canvas and dropdown menu for selecting character

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -3,9 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class CharacterControl : MonoBehaviour
 {
+    public Dropdown selector;
+    public Villager[] characters;
     public TMP_Text textUI;
     public static CharacterControl Instance;
     public static Villager SelectedVillager { get; private set; }
@@ -23,5 +26,9 @@ public class CharacterControl : MonoBehaviour
         SelectedVillager.Selected(true);
         Instance.textUI.text = villager.ToString();
     }
-
+    public void DropdownChange(int value)
+    {
+        // Note: First value in list is left blank. This causes a nullreference error but this is intentional and doesn't break anything
+        SetSelectedVillager(characters[value]);
+    }
 }
